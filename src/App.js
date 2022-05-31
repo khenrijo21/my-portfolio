74c3f2048089535b461af6e527dd192ef3fc5809
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-31 17:06:31 +0000

    Base layout config made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import '../styles/footer.scss'
+
+export default function Footer(){
+      return (
+            <>
+                  
+            </>
+      )
+}
