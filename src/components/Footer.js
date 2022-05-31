e1f9aab02e6fb8a546064f50de57557bc95818b4
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-05-31 17:34:12 +0000

    Footer made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,10 @@ import '../styles/footer.scss'
 
 export default function Footer(){
       return (
-            <>
-                  
-            </>
+            <footer>
+                  <p>Copyright &copy; 2022 -
+                        <a href="https://www.inputstudio.co" target="_blank" rel="noreferrer"> Input Studio</a>
+                  </p>
+            </footer>
       )
 }
