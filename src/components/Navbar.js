ac009e6ec3892314a4f2eb91bac3cfaed95e166b
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-06-01 00:15:14 +0000

    FontAwesome installed and icons added to sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faListCheck, faDiagramProject, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 import '../styles/navbar.scss'
 
 export default function Navbar(){
       return (
-            <>
-                  
-            </>
+            <navbar className="sidebar">
+                  <a href="/" className="active">
+                        <FontAwesomeIcon size="xl" icon={faHome} />
+                  </a>
+                  <a href="/">
+                        <FontAwesomeIcon size="xl" icon={faDiagramProject} />
+                  </a>
+                  <a href="/">
+                        <FontAwesomeIcon size="xl" icon={faListCheck} />
+                  </a>
+                  <a href="/">
+                        <FontAwesomeIcon size="xl" icon={faPaperPlane} />
+                  </a>
+            </navbar>
       )
 }
