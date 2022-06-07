63cb6da381a982e2a895d8806d1807fc5f767440
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-06-07 21:59:17 +0000

    Social links added in about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../styles/components/about-section.scss'
+import { FaGithub, FaLinkedin, FaPhoneAlt } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
 
 export default function AboutSection(){
       return (
@@ -10,8 +12,11 @@ export default function AboutSection(){
                         <div className="about--description">
                               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
-                        <div className="about--btn">
-                              <a href="/" className="know-more">En savoir plus</a>
+                        <div className="about--socials">
+                              <a href="https://github.com/khenrijo21" className="social" target="_blank" rel="noreferrer"><FaGithub /> Github</a>
+                              <a href="/" className="social"><FaLinkedin /> Linkedin</a>
+                              <a href="mailto:khenrijo21@gmail.com" className="social"><AiOutlineMail /> khenrijo21@gmail.com</a>
+                              <a href="tel:+2250747826390" className="social"><FaPhoneAlt /> +225 07 47 82 63 90</a>
                         </div>
                   </div>
                   <div className="about--illustration">
