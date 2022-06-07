be6059e0a9086e36314d18e09a286facbdd1b8d0
Kouassi Henri Joel <kouassikan19@univmetiers.ci>
2022-06-07 21:44:01 +0000

    Skills section finalized

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { SiFlutter } from 'react-icons/si';
 
 import '../styles/components/skill.scss'
 
 export default function Skill({icon, title, desc, percentage}){
       const style = {width: percentage + '%'}
 
+      console.log(icon)
+
       return (
             <div className="skill--wrapper">
                   <div className="skill--header">
-                        <FontAwesomeIcon icon={icon} />
+                        {title === "Flutter" ? <SiFlutter /> :  <FontAwesomeIcon icon={icon} />}
                         <h2>{title}</h2>
                         <p>{desc}</p>
                   </div>
--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -1,32 +1,66 @@
 import React from "react";
-import { faBootstrap, faLaravel } from "@fortawesome/free-brands-svg-icons";
+import { faBootstrap, faLaravel, faReact, faSass } from "@fortawesome/free-brands-svg-icons";
+import { GiSkills } from "react-icons/gi";
+
 import Skill from "./Skill";
 
 import '../styles/components/skills-section.scss'
+import { faDatabase, faListCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function SkillsSection(){
       const skills = [
             {
                   icon: faLaravel,
                   title: "Laravel",
-                  desc: "Framework PHP permettant de creer des webapps, REST API etc...",
+                  desc: "Laravel is an open-source web framework written in PHP respecting the model-view-controller principle and entirely developed in object-oriented programming.",
                   percentage: "95",
+            },
+            {
+                  icon: faSass,
+                  title: "Sass",
+                  desc: "Sass is a preprocessor scripting language that is compiled or interpreted in CSS.",
+                  percentage: "100",
             },
             {
                   icon: faBootstrap,
                   title: "Bootstrap",
-                  desc: "Bibliothèque css",
+                  desc: "Bootstrap is a collection of tools useful for creating web design and web applications. It is a set that contains HTML, CSS and JS code.",
+                  percentage: "100",
+            },
+            {
+                  icon: faReact,
+                  title: "React.js",
+                  desc: "React is a free JavaScript library developed by Facebook since 2013 that facilitates the creation of SPAs through the creation of components.",
+                  percentage: "85",
+            },
+            {
+                  icon: "",
+                  title: "Flutter",
+                  desc: "Flutter is an open-source user interface software development kit created by Google used to develop applications for Android, iOS from a single code base.",
+                  percentage: "60",
+            },
+            {
+                  icon: faDatabase,
+                  title: "SQL (MySQL, PostgreSQL)",
+                  desc: "SQL is a standardised computer language used to operate relational databases.",
+                  percentage: "90",
+            },
+            {
+                  icon: faListCheck,
+                  title: "MERISE",
+                  desc: "Merise is a method of analysis, design and management of IT projects. This method is still suitable for the management of internal projects in organisations, limited to a specific domain.",
                   percentage: "100",
             },
       ]
 
       return (
             <section className="skills--wrapper">
-                  <h1 className="skills-title">Skills</h1>
+                  <h1 className="skills-title">Skills <GiSkills /> </h1>
                   <div className="skills--body">
-                        {skills.map(skill => {
+                        {skills.map((skill, index) => {
                               return (
-                                    <Skill icon={skill.icon}
+                                    <Skill key={index}
+                                          icon={skill.icon}
                                           title={skill.title}
                                           desc={skill.desc}
                                           percentage={skill.percentage} />
